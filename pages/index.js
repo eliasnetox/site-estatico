2e9fdfa3536a136624651a37dd52627557d097ee
eliasnetox <financeiro@androtech.com.br>
2022-03-27 20:33:10 -0300

    head

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
-import Link from 'next/link';
+import Head from 'next/head'
 
-function Home() {
-    return  (<div>
-        <h1>Home</h1>
-
-        <Link href="/politicas">
-            <a>Acessar pagina politicas</a>
-        </Link>
-    </div>)
+function IndexPage() {
+  return (
+    <div>
+      <Head>
+        <title>PRDE</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <p>PRDE HEAD</p>
+    </div>
+  )
 }
 
-export default Home
+export default IndexPage
